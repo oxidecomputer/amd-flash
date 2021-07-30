62ada954442d7b99b26cb2423b5292be23fec90a
Danny Milosavljevic <daym@oxidecomputer.com>
2021-07-30 10:40:36 +0200

    lib: Split Flash into FlashRead, FlashWrite.

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,12 @@ pub enum Error {
 pub type Result<Q> = core::result::Result<Q, Error>;
 pub type Location = u32;
 
-pub trait Flash<const RW_BLOCK_SIZE: usize, const ERASURE_BLOCK_SIZE: usize> {
-    fn read_block(&self, location: Location, buffer: &mut [u8; RW_BLOCK_SIZE]) -> Result<()>;
-    fn write_block(&mut self, location: Location, buffer: &[u8; RW_BLOCK_SIZE]) -> Result<()>;
+pub trait FlashRead<const READING_BLOCK_SIZE: usize> {
+    fn read_block(&self, location: Location, buffer: &mut [u8; READING_BLOCK_SIZE]) -> Result<()>;
+}
+
+pub trait FlashWrite<const WRITING_BLOCK_SIZE: usize, const ERASURE_BLOCK_SIZE: usize> {
+    fn write_block(&mut self, location: Location, buffer: &[u8; WRITING_BLOCK_SIZE]) -> Result<()>;
     fn erase_block(&self, location: Location) -> Result<()>;
 }
 
