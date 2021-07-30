6d06d2461133a7ea9df28ecdcbd9c002a45ab54b
Danny Milosavljevic <daym@oxidecomputer.com>
2021-07-30 12:23:13 +0200

    lib: Add unit test.

--- src/lib.rs
+++ src/lib.rs
@@ -18,31 +18,54 @@ pub trait FlashWrite<const WRITING_BLOCK_SIZE: usize, const ERASURE_BLOCK_SIZE: 
 
 #[cfg(test)]
 mod tests {
-/*
-struct MmioFlash {
+    use super::*;
 
-}
+    struct FlashImage<'a> {
+        buf: &'a mut [u8],
+    }
 
-impl Flash for MmioFlash {
-    fn block_size(&self) -> usize {
-        1
+    impl<'a> FlashImage<'a> {
+        pub fn new(buf: &'a mut [u8]) -> Self {
+            Self {
+                buf
+            }
+        }
     }
-    fn read_block(&self, location: Location, buffer: &mut [u8]) -> Result<()> {
-        Ok(())
-    }
-    fn write_block(&self, location: Location, buffer: &[u8]) -> Result<()> {
-        Ok(())
-    }
-    fn erase_block(&self, location: Location) -> Result<()> {
-        Ok(())
-    }
-}
-*/
 
+    impl FlashRead<0x1000> for FlashImage<'_> {
+        fn read_block(&self, location: Location, buffer: &mut [u8; 0x1000]) -> Result<()> {
+            let block = &self.buf[location as usize .. (location as usize + 0x1000)];
+            buffer[..].copy_from_slice(block);
+            Ok(())
+        }
+    }
 
+    impl FlashWrite<0x1000, 0x2_0000> for FlashImage<'_> {
+        fn write_block(&mut self, location: Location, buffer: &[u8; 0x1000]) -> Result<()> {
+            let block = &mut self.buf[location as usize .. (location as usize + 0x1000)];
+            block.copy_from_slice(&buffer[..]);
+            Ok(())
+        }
+        fn erase_block(&mut self, location: Location) -> Result<()> {
+            let block = &mut self.buf[location as usize .. (location as usize + 0x2_0000)];
+            for e in block.iter_mut() {
+                *e = 0xFF;
+            }
+            Ok(())
+        }
+    }
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn flash_image_usage() -> Result<()> {
+        let mut storage = vec![0xFF; 0x100_0000];
+        let mut flash_image = FlashImage::new(&mut storage[..]);
+        flash_image.write_block(0, &[1u8; 0x1000])?;
+        flash_image.write_block(0x1000, &[2u8; 0x1000])?;
+        let mut buf: [u8; 0x1000] = [0u8; 0x1000];
+        flash_image.read_block(0, &mut buf)?;
+        assert_eq!(buf, [1u8; 0x1000]);
+        flash_image.read_block(0x1000, &mut buf)?;
+        assert_eq!(buf, [2u8; 0x1000]);
+        Ok(())
     }
 }
